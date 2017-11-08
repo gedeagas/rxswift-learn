759e65be12efd251fc270fd0868e69bf693b7f10
Agastya Darma <gedeagas22@gmail.com>
2017-11-08 19:49:00 +0700

    Initial Commit

---
+++ swift-fetch-data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-fetch-data
+//
+//  Created by Agastya Darma on 08/11/17.
+//  Copyright © 2017 Agastya Darma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
